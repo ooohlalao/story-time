67d09a7db4df142315675d0c2cef71cad4700c5e
ooohlalao <namesallbeentaken@gmail.com>
2015-05-31 08:49:10 +0800

    change header and wpadminbar

--- functions.php
+++ functions.php
@@ -14,3 +14,54 @@ function theme_enqueue_styles() {
 //
 // Your code goes below
 //
+
+add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' ); 
+function my_admin_bar_render() { 
+    global $wp_admin_bar; 
+    $wp_admin_bar->add_menu( 
+        array( 'parent' => "top-secondary", // 'false' 为添加住菜单，也可以输入父级菜单的 ID  
+        'id' => 'description', // 自定义链接的 ID，  
+        'title' => get_bloginfo('description'), // 自定义链接标题  
+        'href' => admin_url( '/../'), // 链接地址  
+        'meta' => false // 用来设置自定义链接属性选项的一个数组：array( 'html' => '', 'class' => '', 'onclick' => '', target => '', title => '' );  
+        )
+    );
+	$wp_admin_bar->add_group( array(
+		'id'     => 'top-center',
+		'meta'   => array(
+			'class' => 'ab-top-center',
+		),
+	) );
+    $wp_admin_bar->add_menu( 
+        array( 'parent' => "top-center", // 'false' 为添加住菜单，也可以输入父级菜单的 ID  
+        'id' => 'description', // 自定义链接的 ID，  
+        'title' => get_bloginfo('description'), // 自定义链接标题  
+        'href' => admin_url( '/../'), // 链接地址  
+        'meta' => false // 用来设置自定义链接属性选项的一个数组：array( 'html' => '', 'class' => '', 'onclick' => '', target => '', title => '' );  
+        )
+    );
+}
+
+function mytheme_admin_bar_render() {
+    global $wp_admin_bar;
+    //$wp_admin_bar->remove_menu('search');
+    $wp_admin_bar->remove_menu('wp-logo');
+    $wp_admin_bar->remove_menu('updates');
+    $wp_admin_bar->remove_menu('comments');
+    $wp_admin_bar->remove_menu('new-content');
+}
+add_action('wp_before_admin_bar_render','mytheme_admin_bar_render'); 
+
+
+// 在代码中，我们使用remove_menu(‘comments’)函数来删除“评论”链接，要删除不同的链接或菜单，您可以检查一下/wp-includes/admin-bar.php 这个文件，查找不同链接名称及它们相应的ID。
+// 下面列出其中一部份以供参考：
+
+// my-account                     – 不带头像的个人资料链接
+// my-account-with-avatar  – 带头像的个人资料链接
+// my-blogs                         – 多站点博客中“我的博客”链接
+// get-shortlink                   – 获取简短链接
+// edit                                – 指向“编辑”文章页面的链接
+// new-content                   – “添加新文章”的链接
+// comments                       – “评论”链接
+// appearance                    – “外观”链接
+// updates                         – “更新”链接
--- header.php
+++ header.php
@@ -36,44 +36,57 @@
         
 		<header id="masthead" class="site-header" role="banner">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				
+                    <h1 class="site-title"><?php //bloginfo( 'name' ); ?></h1>
+                    <h2 class="site-description"><?php //bloginfo( 'description' ); ?></h2>
 			</a>
-            
-
-			
 		</header><!-- #masthead -->
         <div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
 					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu nav-menu-small') ); ?>
+                    <table border="0"><tr class="nav-table">
+                    <td class="left">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu nav-menu-l') ); ?>
-                    <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-                    <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+                    </td><td class="center">
+                    <a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                        <img id="logo" src="<?php echo bloginfo ('template_directory');?>/../story-time/images/logo.png" alt="<?php bloginfo ('name');?>" />
+                    </a>
+                    </td><td class="right">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu nav-menu-r') ); ?>
+                    </td>
+                    </tr></table>
                     <script>
                     //remove duplicate navbars
                     var nav_l = document.getElementsByClassName('nav-menu-l');
                     var nav_r = document.getElementsByClassName('nav-menu-r');
-                    removePartial(nav_l,0);//left navbar only remove the even number.
-                    removePartial(nav_r,1);//right navbar only remove the odd.                    
+                    removePartial(nav_l,1);//left navbar removes the former half.
+                    removePartial(nav_r,0);//right navbar removes the second half.                    
                     
-                        //remove item of target from end to header . Removes one item every two.
-                        // if there are six items in total. 6 4 2 or 5 3 1 will be removed individually. 1 3 5 or 2 4 6 will be left.
-                        // if there are five items in total. 4 2 or 5 3 1 will be removed individually. 1 3 5 or 2 4 will be left.
-                        function removePartial(target,shift)
+                        //remove item of target from end to header . Removes the former or later half.
+                        // if there are six items in total. 6 5 4 or 3 2 1 will be removed individually. 1 2 3 or 4 5 6 will be left.
+                        // if there are five items in total. 5 4 or 3 2 1 will be removed individually. 1 2 3 or 4 5 will be left.
+                        function removePartial(target,part)
                         {
                             var ulist = target[0].children[0];
                             var cnt = ulist.childElementCount;
                             
-                            var check = cnt % 2 ; //judge whether cnt is odd.
-                            if(check==1) shift = 1 - shift;//left navbar only remove the even number, right navbar only remove the odd.
-                            
-                            for(var item = cnt-shift ; item > 0; item -= 2)
+                            var halfvalue = Math.round(cnt / 2) ;
+                            var start,end;
+                            if(part==0)
                             {
-                                var chd = ulist.children[item-1];
+                                start = cnt-1; end = halfvalue;
+                            }
+                            else if(part==1)
+                            {
+                                start = halfvalue-1; end = 0;
+                            }
+                            else return;
+                            
+                            for(var item = start ; item > end-1; item -= 1)
+                            {
+                                var chd = ulist.children[item];
                                 ulist.removeChild(chd);
-                                console.log(item-1,cnt);
+                                console.log(item,cnt);
                             }
                         }
                     </script>
